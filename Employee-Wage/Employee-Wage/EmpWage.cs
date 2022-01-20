dc11d34e4585d8156f4684c4f0bf4b7889891190
mahafuztechie <mahafuzaadeez5@gmail.com>
2022-01-20 11:14:51 +0530

    [mahafuz] adding uc4

--- Employee-Wage/Employee-Wage/EmpWage.cs
+++ Employee-Wage/Employee-Wage/EmpWage.cs
@@ -31,34 +31,61 @@ namespace Employee_Wage
         public static void FulltimeOrPartime()
         {
             const int FULL_TIME = 1;
-            const int PART_TIME = 0;
-            int empWagePerHr = 20;
-            int empHr = 0;
+            const int PART_TIME = 2;
             int totalWage = 0;
+            int empHrs = 0;
+            int empWage = 0;
+            const int EMP_RATE_PER_HR = 20;
+            const int MAX_WORKING_DAYS = 20;
+            int day = 1;
             Random random = new Random();
-            int RandomNumber = random.Next(0, 3);
-       
 
-            if (RandomNumber == FULL_TIME)
+            // using for loop to check employee working days with total Wage
+            for (day = 1; day <= MAX_WORKING_DAYS; day++)
             {
-                Console.WriteLine("Employee worked for full time");
-                empHr = 8;
-            }
-            // checking if employee is present part-time
-            else if (RandomNumber == PART_TIME)
-            {
-                Console.WriteLine("Employee worked for part time");
-                empHr = 4;
-            }
-            else
-            {
-                Console.WriteLine("Employee is absent");
-                empHr = 0;
-            }
+                int randomInput = random.Next(0, 3);
 
-            totalWage = empWagePerHr * empHr;
-            Console.WriteLine("Total wage of employee for is" + totalWage);
+               // with iflse
+                //if (RandomNumber == FULL_TIME)
+                //{
+                //    Console.WriteLine("Employee worked for full time");
+                //    empHr = 8;
+                //}
+                //// checking if employee is present part-time
+                //else if (RandomNumber == PART_TIME)
+                //{
+                //    Console.WriteLine("Employee worked for part time");
+                //    empHr = 4;
+                //}
+                //else
+                //{
+                //    Console.WriteLine("Employee is absent");
+                //    empHr = 0;
+                //}
 
+                // switch case
+                switch (randomInput)
+                {
+                    case FULL_TIME:
+                        empHrs = 8;
+                        Console.WriteLine("Employee is present fulltime");
+                        break;
+                    case PART_TIME:
+                        empHrs = 4;
+                        Console.WriteLine("Employee is present parttime");
+                        break;
+                    default:
+                        empHrs = 0;
+                        Console.WriteLine("Employee is absent");
+                        break;
+                }
+
+
+                empWage = EMP_RATE_PER_HR * empHrs;
+                totalWage += empWage;
+                Console.WriteLine("Employee wage for Day {0} is {1}", day, empWage);
+            }
+            Console.WriteLine("total wage is " + totalWage);
         }
 
     }
